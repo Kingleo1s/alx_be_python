023811e1832ef684e907f5562e7bdcfd59d4b07d
Kingleo1s <emmanuelogbodo44@gmail.com>
2025-07-19 14:19:05 +0100

    2nd project

--- python_introduction/basic_operations.py
+++
@@ -1,9 +0,0 @@
-number1 = 10
-number2 = 5
-#getting the addition, subtraction and multiplication of tw numbers
-sum = number1 + number2
-sum2 = number1 - number2
-sum3 = number1 * number2
-print("Addition of", number1, "and", number2, "is", sum)
-print("Subtraction of", number1, "and", number2, "is", sum2)
-print("Multipication of", number1, "and", number2,"is",sum3)
