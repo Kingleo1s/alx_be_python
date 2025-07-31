9e031e80a3d8f968e605b8a4aad743e0d7f53e3d
Emmanuel <89363965+Kingleo1s@users.noreply.github.com>
2025-07-31 13:05:15 +0100

    Update bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+# bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Deposit must be a positive amount.")
+
+    def withdraw(self, amount):
+        if amount > self.__account_balance or amount <= 0:
+            return False
+        self.__account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Current account balance: ₦{self.__account_balance:,.2f}")
